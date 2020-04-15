21e2515f874ea1252467677a0bf24a2d009b5444
nairsujitk <40444156+nairsujitk@users.noreply.github.com>
2020-04-15 12:31:29 +0530

    Update 04 kubernetes.tf

--- 04 kubernetes.tf
+++ 04 kubernetes.tf
@@ -1,17 +1,17 @@
 data "google_client_config" "default" {}
 
 data "google_container_cluster" "gke-cluster" {
-    name = var.cluster_name
-    location = var.region_zone 
+    name = "${var.cluster_name}"
+    location = "${var.region_zone}"
 }
 
 provider "kubernetes" {
     version = ">= 1.10.0"
     load_config_file = false
     
-    host = google_container_cluster.gke-cluster.endpoint
+    host = "${google_container_cluster.gke-cluster.endpoint}"
     
-    token = data.google_client_config.default.access_token
+    token = "${data.google_client_config.default.access_token}"
 
     client_certificate = base64decode(google_container_cluster.gke-cluster.master_auth[0].client_certificate)
     client_key = base64decode(google_container_cluster.gke-cluster.master_auth[0].client_key)
