7b5294ad92274ca2cd917f170f871a47ec0a1c4a
nairsujitk <40444156+nairsujitk@users.noreply.github.com>
2020-04-15 12:46:27 +0530

    Update 05 load_kubeconfig.tf

--- 05 load_kubeconfig.tf
+++ 05 load_kubeconfig.tf
@@ -2,12 +2,12 @@ resource "null_resource"  "configure_kubectl" {
   provisioner "local-exec" {
     command = "gcloud container clusters get-credentials ${google_container_cluster.gke-cluster.name} --zone ${var.region_zone} --project ${var.project_name}"
   }
-  depends_on = [google_container_cluster.gke-cluster]
+  depends_on = ["google_container_cluster.gke-cluster"]
 }
 
 resource "null_resource"  "delay_pod_creation" {
   provisioner "local-exec" {
     command = "sleep 20"
   }
-  depends_on = [null_resource.configure_kubectl]
+  depends_on = ["null_resource.configure_kubectl"]
 }
