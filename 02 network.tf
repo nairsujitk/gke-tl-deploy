ef8c635a4f8022bd0602eba6f25dda8bcc240c7b
nairsujitk <40444156+nairsujitk@users.noreply.github.com>
2020-04-15 12:27:53 +0530

    Update 02 network.tf

--- 02 network.tf
+++ 02 network.tf
@@ -1,8 +1,8 @@
 resource "google_compute_subnetwork" "gkelan-net" {
   name          = "gkelan-net"
   ip_cidr_range = "10.1.1.0/24"
-  network       = google_compute_network.gkelan.self_link
-  region        = var.region
+  network       = "${google_compute_network.gkelan.self_link}"
+  region        = "${var.region}"
 }
 
 resource "google_compute_network" "gkelan" {
