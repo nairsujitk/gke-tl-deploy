fbd90e515a63b4bcd6eee5b3b83dcdddd43ea3d3
nairsujitk <40444156+nairsujitk@users.noreply.github.com>
2020-04-15 12:36:12 +0530

    Update 09 output.tf

--- 09 output.tf
+++ 09 output.tf
@@ -1,14 +1,14 @@
 output "Voting_APP-ip" {
-    value = kubernetes_service.voting_service.load_balancer_ingress[0].ip
+    value = "${kubernetes_service.voting_service.load_balancer_ingress[0].ip}"
 }
 
 output "Voting_Result-ip" {
-    value = kubernetes_service.result_service.load_balancer_ingress[0].ip
+    value = "${kubernetes_service.result_service.load_balancer_ingress[0].ip}"
 }
 
 
 locals {
-    console_mgmt_ip = kubernetes_service.twistlock_console.load_balancer_ingress[0].ip
+    console_mgmt_ip = "${kubernetes_service.twistlock_console.load_balancer_ingress[0].ip}"
 }
 
 output "PCC_Console-IP" {
@@ -16,8 +16,8 @@ output "PCC_Console-IP" {
 }
 
 output "PCC_Conslole-Username" {
-    value = var.username
+    value = "${var.username}"
 }
 output "PCC_Conslole-Password" {
-    value = var.password
+    value = "${var.password}"
 }
